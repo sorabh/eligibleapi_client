654abe1ea213cbb61469f0eecff36d2523423ec7
sorabh <sorabh@sorabh-desktop.(none)>
2013-04-11 23:53:57 +0530

    putting convention

--- serviceListResponse.py
+++ serviceListResponse.py
@@ -4,6 +4,6 @@ from pprint import pprint
 class serviceListResponse:
     def __init__(self,json_str):
         self.json_str=json_str
-    def Print(self):
+    def print_data(self):
         pprint(self.json_str)
 
